e5ac570b1c0ebd67c7b9de157877382a7dbc1807
Paul Tarau <paul@kyndi.com>
2017-08-15 20:37:57 -0500

    adding IntelliJ project files

--- IP/clean.sh
+++ IP/clean.sh
@@ -1,4 +1,4 @@
-#echo go.sh <name_of_prog> assumed in dir ./progs
+# usage: go.sh <name_of_prog> assumed in dir ./progs
 export TARGET="out/production/IP"
 rm -r -f $TARGET/iProlog
 rm -f progs/*.pl.nl
