2db0d3387318964f81a09b5f4084694a0c028d92
zachary eisenhauer <zachary eisenhauer>
2017-10-24 02:45:58 -0500

    Worked on Toks.js, researched and understand tokenizers better

--- JS/src/Toks.js
+++ JS/src/Toks.js
@@ -1,11 +1,14 @@
+
+
 // Should this extend something? What does StreamTokenizer do?
 var Toks = class Toks {
-  var IF = "if";
-  var AND = "and";
-  var DOT = ".";
-  var HOLDS = "holds";
-  var LISTS = "lists";
-  var IS = "is";
+
+  IF = "if";
+  AND = "and";
+  DOT = ".";
+  HOLDS = "holds";
+  LISTS = "lists";
+  IS = "is";
 
   /*
   * This function takes a string and reads it into a variable
@@ -14,16 +17,25 @@ var Toks = class Toks {
   *
   * @param s - a string
   * @param fromFile - boolean to determine if the string is from a file
+  *
+  * https://github.com/Floby/node-tokenizer
+  *
   */
   function makeToks(s, fromFile){
-
-    if(fromFile){
-      // Read s from a file
-    }
-    else{
-      // Read s like a string
-    }
-    var T = new Toks(R);
+	  
+	// Read s from a file
+	s = require('fs');
+	s.readFile('/' + fromFile, 'utf8', function(err,data){
+    if(err){
+      return console.log(err);
+      }
+      else{
+        console.log(data);
+      }
+  });
+	
+    
+    var T = new Toks(s);
     return T;
   }
 
@@ -33,6 +45,119 @@ var Toks = class Toks {
   *
   * @param R - The read in string from makeToks
   */
-  Toks(R){
+  function Toks(s){
+    var Tokenizer = require('tokenizer');
+    var t = new Tokenizer(mycallback);
+
+    t.addRule(/^.$/, 'ordinary');
+    t.addRule(/^!$/, 'ordinary');
+    t.addRule(/^"$/, 'ordinary');
+    t.addRule(/^#$/, 'ordinary');
+    t.addRule(/^\$$/, 'ordinary');
+    t.addRule(/^%$/, 'ordinary');
+    t.addRule(/^&$/, 'ordinary');
+    t.addRule(/^'$/, 'ordinary');
+    t.addRule(/^\($/, 'ordinary');
+    t.addRule(/^\)$/, 'ordinary');
+    t.addRule(/^*$/, 'ordinary');
+    t.addRule(/^+$/, 'ordinary');
+    t.addRule(/^,$/, 'ordinary');
+    t.addRule(/^-$/, 'ordinary');
+    t.addRule(/^\/$/, 'ordinary');
+    t.addRule(/^:$/, 'ordinary');
+    t.addRule(/^;$/, 'ordinary');
+    t.addRule(/^<$/, 'ordinary');
+    t.addRule(/^=$/, 'ordinary');
+    t.addRule(/^>$/, 'ordinary');
+    t.addRule(/^?$/, 'ordinary');
+    t.addRule(/^@$/, 'ordinary');
+    t.addRule(/^\[$/, 'ordinary');
+    t.addRule(/^\\$/, 'ordinary');
+    t.addRule(/^]$/, 'ordinary');
+    t.addRule(/^\^$/, 'ordinary');
+    t.addRule(/^_$/, 'ordinary');
+    t.addRule(/^`$/, 'ordinary');
+    t.addRule(/^{$/, 'ordinary');
+    t.addRule(/^\|$/, 'ordinary');
+    t.addRule(/^}$/, 'ordinary');
+    t.addRule(/^~$/, 'ordinary');
+
+    t.addRule(/^a$/, 'wordchars');
+    t.addRule(/^b$/, 'wordchars');
+    t.addRule(/^c$/, 'wordchars');
+    t.addRule(/^d$/, 'wordchars');
+    t.addRule(/^e$/, 'wordchars');
+    t.addRule(/^f$/, 'wordchars');
+    t.addRule(/^g$/, 'wordchars');
+    t.addRule(/^h$/, 'wordchars');
+    t.addRule(/^i$/, 'wordchars');
+    t.addRule(/^j$/, 'wordchars');
+    t.addRule(/^k$/, 'wordchars');
+    t.addRule(/^l$/, 'wordchars');
+    t.addRule(/^m$/, 'wordchars');
+    t.addRule(/^n$/, 'wordchars');
+    t.addRule(/^o$/, 'wordchars');
+    t.addRule(/^p$/, 'wordchars');
+    t.addRule(/^q$/, 'wordchars');
+    t.addRule(/^r$/, 'wordchars');
+    t.addRule(/^s$/, 'wordchars');
+    t.addRule(/^t$/, 'wordchars');
+    t.addRule(/^u$/, 'wordchars');
+    t.addRule(/^v$/, 'wordchars');
+    t.addRule(/^w$/, 'wordchars');
+    t.addRule(/^x$/, 'wordchars');
+    t.addRule(/^y$/, 'wordchars');
+    t.addRule(/^z$/, 'wordchars');
+
+    t.addRule(/^A$/, 'wordchars');
+    t.addRule(/^B$/, 'wordchars');
+    t.addRule(/^C$/, 'wordchars');
+    t.addRule(/^D$/, 'wordchars');
+    t.addRule(/^E$/, 'wordchars');
+    t.addRule(/^F$/, 'wordchars');
+    t.addRule(/^G$/, 'wordchars');
+    t.addRule(/^H$/, 'wordchars');
+    t.addRule(/^I$/, 'wordchars');
+    t.addRule(/^J$/, 'wordchars');
+    t.addRule(/^K$/, 'wordchars');
+    t.addRule(/^L$/, 'wordchars');
+    t.addRule(/^M$/, 'wordchars');
+    t.addRule(/^N$/, 'wordchars');
+    t.addRule(/^O$/, 'wordchars');
+    t.addRule(/^P$/, 'wordchars');
+    t.addRule(/^Q$/, 'wordchars');
+    t.addRule(/^R$/, 'wordchars');
+    t.addRule(/^S$/, 'wordchars');
+    t.addRule(/^T$/, 'wordchars');
+    t.addRule(/^U$/, 'wordchars');
+    t.addRule(/^V$/, 'wordchars');
+    t.addRule(/^W$/, 'wordchars');
+    t.addRule(/^X$/, 'wordchars');
+    t.addRule(/^Y$/, 'wordchars');
+    t.addRule(/^Z$/, 'wordchars');
+    
+    t.addRule(/^0$/, 'wordchars');
+    t.addRule(/^1$/, 'wordchars');
+    t.addRule(/^2$/, 'wordchars');
+    t.addRule(/^3$/, 'wordchars');
+    t.addRule(/^4$/, 'wordchars');
+    t.addRule(/^5$/, 'wordchars');
+    t.addRule(/^6$/, 'wordchars');
+    t.addRule(/^7$/, 'wordchars');
+    t.addRule(/^8$/, 'wordchars');
+    t.addRule(/^9$/, 'wordchars');
+   
+   
+    t.addRule(/^\/\/$/, 'comment');
+    t.addRule(/^\*$/, 'comment');
+    t.addRule(/^*\/$/, 'comment');
+	  
+  }
+
+  function getWord(){
+    var t = "";
+    
+    var eos = require('end-of-stream');
+    
   }
 }
