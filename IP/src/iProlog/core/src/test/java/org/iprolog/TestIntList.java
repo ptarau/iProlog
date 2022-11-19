266cc372b32234169a0546fc272e7ce2a54c27e3
Michael Turner <michael.eugene.turner@gmail.com>
2022-11-19 09:05:33 +0900

    Added more IntList unit tests

--- IP/src/iProlog/core/src/test/java/org/iprolog/TestIntList.java
+++ IP/src/iProlog/core/src/test/java/org/iprolog/TestIntList.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import org.iprolog.IntList;
 
@@ -11,10 +12,30 @@ public class TestIntList {
 
     @Test
     public void mainTest() {
-        assertEquals(1,1);
         System.out.println ("<<<<<<< TestIntList >>>>>>>>");
+
+
         IntList il = IntList.cons(1,null);
         assertNotNull(il);
+        assertFalse(IntList.isEmpty(il));
+        assertEquals(1,IntList.head(il));
+        assertEquals(1,IntList.len(il));
+
+        IntList il2 = IntList.cons(2,il);
+        assertNotNull(il2);
+        assertFalse(IntList.isEmpty(il2));
+        assertEquals(2,IntList.head(il2));
+
+        final int [] xs = { 3, 4 };
+        assertEquals(2,xs.length);
+        assertEquals(2,IntList.len(il2));
+        IntList ilx = IntList.app(xs,il2);
+        assertEquals(4,IntList.len(ilx));
+
+        System.out.println ("ilx = " + ilx.toString());
+
+        IntStack is = IntList.toInts(ilx);
+        System.out.println ("is = " + is.toString());
         System.out.println ("<<<<<<< TestIntList DONE >>>>>>>>");
     }
 
