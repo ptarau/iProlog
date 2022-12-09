9f5eb5966bf2345290dff01141e3a999229d1d71
Michael Turner <michael.eugene.turner@gmail.com>
2022-12-09 21:38:55 +0900

    Code comments & rearrangement; new test module

---
+++ IP/src/iProlog/core/src/test/java/org/iprolog/TestIMap.java
@@ -0,0 +1,17 @@
+
+package org.iprolog;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class TestIMap {
+    @Test
+    public void mainTest() {
+        System.out.println("TestIMap entered");
+    }
+}
+
+
