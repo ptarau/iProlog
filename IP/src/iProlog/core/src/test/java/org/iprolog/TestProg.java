b9b4a761be9e65e30e58ae3762839b3b839408ea
Michael Turner <michael.eugene.turner@gmail.com>
2022-12-13 18:30:44 +0900

    Touchups

---
+++ IP/src/iProlog/core/src/test/java/org/iprolog/TestProg.java
@@ -0,0 +1,15 @@
+package org.iprolog;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+public class TestProg {
+    @Test
+    public void mainTest() {
+        System.out.println("entering TestProg");
+        System.out.println("exiting TestProg");
+    }
+}
