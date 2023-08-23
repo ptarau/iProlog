b4b700e834e66514dd157e5ad12c56236662ebd3
Michael Turner <michael.eugene.turner@gmail.com>
2023-08-24 00:54:49 +0900

    Add test converted to better API

--- IP/src/test/java/org/iprolog/TestAdd.java
+++ IP/src/test/java/org/iprolog/TestAdd.java
@@ -3,28 +3,31 @@ package org.iprolog;
 import org.junit.jupiter.api.Test;
 
 public class TestAdd extends TestTerm {
-    Term  the_sum_of(Term a1, Term a2, Term Sum)    {  return s_(m_(), a1,a2,Sum);  }
-    Term  the_successor_of(Term x)                  {  return s_(m_(), x);          }
+
+    LPvar  the_sum_of(LPvar a1, LPvar a2, LPvar Sum)    {  return S_(a1,a2,Sum);  }
+    LPvar  the_successor_of(LPvar x)                    {  return S_(x);          }
+    LPvar R;
+    LPvar X,Y,Z;
+    LPvar  goal(LPvar x)                                {  return S_(x);          }
 
     @Test
     public void mainTest() {
         start_new_test();
 
-        Term succ_X = the_successor_of(X());
-        Term succ_Z = the_successor_of(Z());
-        Term one    = the_successor_of(c0());
-        Term vR = v_("R");
+        LPvar zero = C_("0");
+        LPvar one = the_successor_of(zero);
+        LPvar two = the_successor_of(one);
 
-        say_( the_sum_of(c0(), X(), X()));
-        say_( the_sum_of(succ_X, Y(), succ_Z)).if_(the_sum_of(X(),Y(),Z()));
+        say_( the_sum_of(zero, X, X));
+        say_( the_sum_of(the_successor_of(X), Y, the_successor_of(Z)) )
+                .if_( the_sum_of(X,Y,Z) );
 
-        Term two = the_successor_of(one);
-        say_( goal(vR)).if_(the_sum_of(two, two, vR));
+        say_( goal(R)).if_(the_sum_of(two, two, R));
 
-        String[] these_answers = {
+        String[] answer = {
                 "the_successor_of(the_successor_of(the_successor_of(the_successor_of(0))))"
         };
 
-        try_it(said, these_answers);
+        try_it(said, answer);
     }
 }
--- IP/src/test/java/org/iprolog/TestJapaneseCode.java
+++ IP/src/test/java/org/iprolog/TestJapaneseCode.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 public class TestJapaneseCode extends TestTerm {
         LPvar いきる(LPvar x)    {  return S_(x);      }
         LPvar  いいです(LPvar x) {  return S_(x);      }
+        LPvar いい(LPvar x)     {  return S_(x);      }
         LPvar goal(LPvar x)    {  return S_(x);      }
         LPvar 人;
 
@@ -13,9 +14,11 @@ public class TestJapaneseCode extends TestTerm {
             start_new_test();
 
             String expected[] = {"私", "あなた"};
-            for (String s : expected)
-                say_(いきる (C_(s)));
-            say_(いいです (人)).if_( いきる (人));
+            for (String s : expected) {
+                say_(いきる(C_(s)));
+                say_(いい(C_(s)));
+            }
+            say_(いいです (人)).if_( いきる (人),いい(人));
             say_(goal(人)).if_(いいです (人));
 
             try_it(said, expected);
