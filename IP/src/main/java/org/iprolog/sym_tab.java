8eab049f3acb5be2ad844e42df15c0fe8816d1eb
Michael Turner <michael.eugene.turner@gmail.com>
2024-03-15 12:29:38 +0900

    Optional int rather than new Integer in sym_tab

--- IP/src/main/java/org/iprolog/sym_tab.java
+++ IP/src/main/java/org/iprolog/sym_tab.java
@@ -4,18 +4,39 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
 public class sym_tab {
-    /* Symbol table - made of map (syms) + reverse map from ints to syms (slist) */
+    // Symbol table - made of map (syms) + reverse map from ints to syms (slist) */
+
+    private final ArrayList<String> slist;     // resizeable ints->sym
+/*
+    //  linear search only to test whether just "int" is enough
+    // for the logic of iProlog execution.
+    // Sadly, "LinkedHashMap<String, int> syms" is disallowed.
+    // And yes, it turns out iProlog can do without "new Integer" here.
+    sym_tab() {
+        slist = new ArrayList<String>();
+    }
+
+    public final int addSym(final String sym) {
+        int last_size = slist.size();
+        for (int i = 0; i < last_size; ++i)
+            if (sym.equals(slist.get(i)))
+                return i;
+
+        slist.add(sym);
+        return last_size;
+    }
+*/
+
     private final LinkedHashMap<String, Integer> syms; // syms->ints
-    private final ArrayList<String> slist;     // resizeable ints->syms
 
     sym_tab() {
         syms = new LinkedHashMap<String, Integer>();
         slist = new ArrayList<String>();
     }
 
-    /**
-     * Places an identifier in the symbol table.
-     */
+    //
+    //Places an identifier in the symbol table.
+    //
     public final int addSym(final String sym) {
         Integer I = syms.get(sym);
         if (null == I) {
@@ -27,14 +48,13 @@ public class sym_tab {
         return I.intValue();
     }
 
-    /**
-     * Returns the symbol associated to an integer index
-     * in the symbol table.
-     */
+    //
+    // Returns the symbol associated to an integer index
+    //in the symbol table.
+    //
     public final String getSym(final int w) {
         if (w < 0 || w >= slist.size())
             return "BADSYMREF=" + w;
         return slist.get(w);
     }
-
 }
